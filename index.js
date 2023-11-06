55b34b3d5cf90583f15ca338b8a3351280756ba4
Elijah <ekollie324@gmail.com>
2023-11-06 17:08:09 -0500

    3/3 tests passed

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-
-
 function addId(el) {
-    const li = document.createElement('li')
-    li.textContent = el
-    document.body.append(li)
+  const li = document.createElement("li");
+  li.textContent = el;
+  document.body.append(li);
 }
 
 function submitData(nameEl, emailEl) {
-    const url = "http://localhost:3000/users"
+  const url = "http://localhost:3000/users";
 
-    // Make POST request to fill users with data 
-    // 1) Get the data we want to POST
-    // 2) Set up the fetch to make a post 
-    // 3) Call invoke it to do the action 
+  // Make POST request to fill users with data
+  // 1) Get the data we want to POST
+  // 2) Set up the fetch to make a post
+  // 3) Call invoke it to do the action
 
-    let userObject = {
-        "name": nameEl,
-        "email": emailEl,
-    }
+  let userObject = {
+    name: nameEl,
+    email: emailEl,
+  };
 
-    return fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": 'application/json',
-            "Accept": "application/json",
-        },
-        body: JSON.stringify(userObject)
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userObject),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      addId(data.id);
     })
-        .then((res) => res.json())
-        .then((data) => {
-            addId(data.id)
-        })
+    .catch((res) => addId(res));
 }
